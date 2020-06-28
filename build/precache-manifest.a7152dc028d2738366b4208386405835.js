c4f6fce9102b14c258736fc117e652c3c96a793b
Lam <lamdayap@gmail.com>
2020-06-28 21:12:25 +0800

    added my build

---
+++ build/precache-manifest.a7152dc028d2738366b4208386405835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5212895bc5e56e5794dab2ffcee4b0fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac8e1f0c3e7d8bf4240",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac8e1f0c3e7d8bf4240",
+    "url": "/static/js/main.80b20dcc.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
